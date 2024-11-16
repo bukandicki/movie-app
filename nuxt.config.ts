4795724693335955cd6c17f1a1b930aa188ce0c4
Dicki Maulana <dickimaulanawebdev@gmail.com>
2024-11-16 20:45:11 +0700

    chore: install sass

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@nuxt/image'],
+
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: "modern-compiler",
+        },
+      },
+    },
+  },
+
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
-  modules: ['@nuxt/image']
+  devtools: { enabled: true }
 })
