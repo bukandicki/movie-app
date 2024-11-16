a7e01bddfc66082dd646ae8c426fec355bb9755b
Dicki Maulana <dickimaulanawebdev@gmail.com>
2024-11-16 21:14:26 +0700

    feat: Create Nuxt Font Configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/image'],
+  modules: ["@nuxt/image", "@nuxt/fonts"],
 
   vite: {
     css: {
@@ -12,8 +12,50 @@ export default defineNuxtConfig({
     },
   },
 
+  app: {
+    head: {
+      htmlAttrs: { lang: "en" },
+      link: [
+        {
+          rel: "preload",
+          fetchpriority: "high",
+          crossorigin: "anonymous",
+          type: "font/woff2",
+          href: "/fonts/Khand-Bold.woff2",
+          as: "font",
+        },
+        {
+          rel: "preload",
+          fetchpriority: "high",
+          crossorigin: "anonymous",
+          type: "font/woff2",
+          href: "/fonts/Switzer-Regular.woff2",
+          as: "font",
+        },
+      ],
+    },
+  },
+
+  fonts: {
+    defaults: { styles: ["normal"] },
+    families: [
+      {
+        name: "Khand",
+        src: "/fonts/Khand-Bold.woff2",
+        global: true,
+        weight: 600,
+      },
+      {
+        name: "Switzer",
+        src: "/fonts/Switzer-Regular.woff2",
+        global: true,
+        weight: 400,
+      },
+    ],
+  },
+
   css: ["~/assets/scss/_preflight.scss"],
 
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+});
