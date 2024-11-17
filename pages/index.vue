c755d17ec71e17f9d1d63805f4c6f6e7fc1ac295
Dicki Maulana <dickimaulanawebdev@gmail.com>
2024-11-17 17:37:33 +0700

    feat: init home page structure

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,52 @@
 <script lang="ts" setup></script>
 
 <template>
-  <main class="Home"></main>
+  <main class="Home">
+    <section class="Home__hero">
+      <NuxtImg
+        class="Hero__img"
+        src="/images/movies/peakpx.jpg"
+        preload
+        format="webp"
+        alt=""
+        sizes=""
+      />
+
+      <div class="Hero__awards"></div>
+
+      <div class="Hero__detail">
+        <div class="Hero__information">
+          <h1 class="Hero__title"></h1>
+
+          <p class="Hero__description"></p>
+        </div>
+
+        <div class="Hero__galeries"></div>
+      </div>
+    </section>
+
+    <section class="Home__voted">
+      <div class="Voted__headline">
+        <h2>VOTED MOVIES</h2>
+
+        <p>The movies that stole the spotlight!</p>
+      </div>
+
+      <ul class="VotedMovies">
+        <li class="VotedMovies__item">
+          <LazyMovieCard />
+        </li>
+      </ul>
+    </section>
+
+    <section class="Home__movies">
+      <ul class="Movies">
+        <li class="Movies__item">
+          <LazyMovieCard />
+        </li>
+      </ul>
+    </section>
+  </main>
 </template>
 
 <style lang="scss">
