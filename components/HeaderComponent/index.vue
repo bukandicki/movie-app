983cf88046a7f253fba411f0015ff2706289b282
Dicki Maulana <dickimaulanawebdev@gmail.com>
2024-11-17 21:35:55 +0700

    feat: create header component

---
+++ components/HeaderComponent/index.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup></script>
+
+<template>
+  <header class="Header">
+    <div class="Header__brand">
+      <strong>DMOVIES</strong>
+    </div>
+
+    <nav class="Header__nav">
+      <ul>
+        <li><NuxtLink href="/">MOVIES</NuxtLink></li>
+      </ul>
+    </nav>
+
+    <div class="Header__search">
+      <IconSearch />
+
+      <input type="text" placeholder="Search Movies..." />
+    </div>
+  </header>
+</template>
+
+<style lang="scss">
+@use "./HeaderComponent.styles.scss" as *;
+</style>
