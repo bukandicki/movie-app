6860a87426a946877d4149cac2c70908e13680cc
Dicki Maulana <dickimaulanawebdev@gmail.com>
2024-11-18 23:07:06 +0700

    improve movie store

--- stores/movie.ts
+++ stores/movie.ts
@@ -1,6 +1,7 @@
 import type { API_RESPONSE_TYPE, FILTER_TYPE, MOVIE_TYPE } from "~/lib/types";
 
 export const useMovieStore = defineStore("movie_store", () => {
+  const movie_detail = useState<MOVIE_TYPE | null>("movie_detail", () => null);
   const movies = useState<API_RESPONSE_TYPE<MOVIE_TYPE[]>>("movies", () => ({
     data: [],
     message: "",
@@ -14,14 +15,70 @@ export const useMovieStore = defineStore("movie_store", () => {
   async function FETCH_MOVIES(): Promise<
     API_RESPONSE_TYPE<MOVIE_TYPE[]> | undefined
   > {
-    const data = await $fetch<API_RESPONSE_TYPE<MOVIE_TYPE[]>>("/api/movies", {
-      query: movie_filter.value,
-    });
+    try {
+      const data = await $fetch<API_RESPONSE_TYPE<MOVIE_TYPE[]>>(
+        "/api/movies",
+        {
+          query: movie_filter.value,
+        }
+      );
 
-    movies.value = data;
+      movies.value = data;
 
-    return data;
+      return data;
+    } catch (error) {
+      console.error(error);
+    }
   }
 
-  return { movies, movie_filter, FETCH_MOVIES };
+  async function FETCH_MOVIE_DETAIL(
+    id: number
+  ): Promise<MOVIE_TYPE | undefined> {
+    try {
+      const data = await $fetch<API_RESPONSE_TYPE<MOVIE_TYPE>>(
+        `/api/movies/${id}`
+      );
+
+      movie_detail.value = data.data;
+
+      return data.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async function CREATE_MOVIE(payload: Omit<MOVIE_TYPE, "id">) {
+    try {
+      await $fetch("/api/movies", {
+        method: "POST",
+        body: JSON.stringify(payload),
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async function UPDATE_MOVIE(
+    id: number,
+    payload: Partial<Omit<MOVIE_TYPE, "id">>
+  ) {
+    try {
+      await $fetch(`/api/movies/${id}`, {
+        method: "PATCH",
+        body: JSON.stringify(payload),
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return {
+    movies,
+    movie_detail,
+    movie_filter,
+    FETCH_MOVIES,
+    CREATE_MOVIE,
+    UPDATE_MOVIE,
+    FETCH_MOVIE_DETAIL,
+  };
 });
