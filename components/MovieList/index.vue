6afcc1bda742f15daa3e3d140c3fdbc12cded044
Dicki Maulana <dickimaulanawebdev@gmail.com>
2024-11-18 18:41:46 +0700

    apply pagination

--- components/MovieList/index.vue
+++ components/MovieList/index.vue
@@ -1,8 +1,17 @@
 <script lang="ts" setup>
+import type { API_RESPONSE_TYPE, MOVIE_TYPE } from "~/lib/types";
+
 defineProps<{
+  data: API_RESPONSE_TYPE<MOVIE_TYPE[]>;
   title: string;
   description: string;
+  currentPage: number;
   withSearch?: boolean;
+}>();
+
+const emit = defineEmits<{
+  onSearch: [value: string];
+  pageChanged: [value: number];
 }>();
 </script>
 
@@ -17,15 +26,26 @@ defineProps<{
       </div>
 
       <div class="MovieList__search" v-if="withSearch">
-        <input type="text" placeholder="Search movies..." />
+        <input
+          type="text"
+          placeholder="Search movies..."
+          @input="($event) => emit('onSearch', ($event.target as HTMLInputElement).value)"
+        />
       </div>
     </div>
 
     <ul class="MovieList__lists">
-      <li v-for="i in 20" :key="i" class="MovieList__item">
-        <MovieCard />
+      <li v-for="movie in data.data" :key="movie.id" class="MovieList__item">
+        <MovieCard :data="movie" />
       </li>
     </ul>
+
+    <Pagination
+      @page-changed="emit('pageChanged', $event)"
+      :current="currentPage"
+      :total_pages="(data.total_pages as number)"
+      :visible="5"
+    />
   </section>
 </template>
 
